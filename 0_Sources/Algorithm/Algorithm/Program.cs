4bc14b5f042ec2f5a42cbfc387ee72d9b56a3868
Pyunghwa Kim <syneffort@gmail.com>
2020-04-22 21:59:55 +0900

    Initial commit

---
+++ 0_Sources/Algorithm/Algorithm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                // 입력
+
+                // 로직
+
+                // 렌더링
+
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Hello World!");
+            }
+
+            
+        }
+    }
+}
