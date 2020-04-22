43d5ed9e6fb8393aef1364edeec0ddcd34892aed
Pyunghwa Kim <syneffort@gmail.com>
2020-04-23 07:16:09 +0900

    envronmet setting

---
+++ 0_Sources/Algorithm/Algorithm/Board.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class Board
+    {
+        public int[] _data = new int[25]; // 배열
+        public List<int> _data2 = new List<int>(); // 동적 배열
+        public LinkedList<int> _data3 = new LinkedList<int>(); // 이중 연결 동적 배열
+
+        public void Initialize()
+        {
+
+        }
+
+    }
+}
--- 0_Sources/Algorithm/Algorithm/Program.cs
+++ 0_Sources/Algorithm/Algorithm/Program.cs
@@ -8,9 +8,21 @@ namespace Algorithm
         {
             Console.CursorVisible = false;
 
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+
+            int lastTick = 0;
             while (true)
             {
+                #region 프레임 관리
                 // FPS 프레임 (60프레임 OK, 30프레임 이하 NG)
+                int currentTick = System.Environment.TickCount;
+
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+
+                lastTick = currentTick;
+                #endregion
 
                 // 입력
 
@@ -19,7 +31,16 @@ namespace Algorithm
                 // 렌더링
 
                 Console.SetCursorPosition(0, 0);
-                Console.WriteLine("Hello World!");
+                
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
             }
 
             
