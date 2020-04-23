52b36300e7f9cb70bfd0deaa110952303fce7301
Pyunghwa Kim <syneffort@gmail.com>
2020-04-24 08:15:14 +0900

    Binary Tree 미로 생성

--- 0_Sources/Algorithm/Algorithm/Board.cs
+++ 0_Sources/Algorithm/Algorithm/Board.cs
@@ -23,17 +23,64 @@ namespace Algorithm
 
         public void Initialize(int size)
         {
+            if (size % 2 == 0)
+                return;
+
             _size = size;
             _tile = new TileType[_size, _size];
 
+            GenerateByBinaryTree();
+        }
+
+        void GenerateByBinaryTree()
+        {
+
+            // 길을 막는 작업
             for (int y = 0; y < _size; y++)
             {
                 for (int x = 0; x < _size; x++)
                 {
-                    if (x == 0 || x == _size - 1 || y == 0 || y == size - 1)
+                    if (x % 2 == 0 || y % 2 == 0)
                         _tile[y, x] = TileType.Wall;
                     else
                         _tile[y, x] = TileType.Empty;
+                }
+            }
+
+            // 랜덤으로 우측 혹은 아래로 길을 열어주는 작업
+            // 바이너리 트리 알고리즘
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                    }
+
                 }
             }
         }
@@ -60,11 +107,11 @@ namespace Algorithm
             switch (type)
             {
                 case TileType.Empty:
-                    return ConsoleColor.Green;
+                    return ConsoleColor.Black;
                 case TileType.Wall:
                     return ConsoleColor.Gray;
                 default:
-                    return ConsoleColor.Green;
+                    return ConsoleColor.Black;
             }
         }
     }
