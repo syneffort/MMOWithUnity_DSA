315e19be041cdc5d3e0ab3532f7285438090e834
Pyunghwa Kim <syneffort@gmail.com>
2020-05-01 00:04:17 +0900

    [#11] DFS

--- 0_Sources/Algorithm/Exercise/Program.cs
+++ 0_Sources/Algorithm/Exercise/Program.cs
@@ -12,8 +12,8 @@ namespace Exercise
                 { 0, 1, 0, 1, 0, 0 },
                 { 1, 0, 1, 1, 0, 0 },
                 { 0, 1, 0, 0, 0, 0 },
-                { 1, 1, 0, 0, 1, 0 },
-                { 0, 0, 0, 1, 0, 1 },
+                { 1, 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1 },
                 { 0, 0, 0, 0, 1, 0 },
             };
 
@@ -22,10 +22,54 @@ namespace Exercise
                 new List<int>() { 1, 3 },
                 new List<int>() { 0, 2, 3},
                 new List<int>() { 1 },
-                new List<int>() { 0, 1, 4 },
-                new List<int>() { 3, 5 },
+                new List<int>() { 0, 1 },
+                new List<int>() { 5 },
                 new List<int>() { 4, },
             };
+
+            bool[] visited = new bool[6];
+            // 1) now 방문함
+            // 2) now와 연결된 버텍스들을 하나씩 확인해서 아직 미방문 상태라면 방문함
+            public void DFS(int now)
+            {
+                Console.WriteLine(now);
+                visited[now] = true;
+
+                for (int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == 0) // 연결되지 않았으면 컨티뉴
+                        continue;
+
+                    if (visited[next]) // 이미 방문했다면 컨티뉴
+                        continue;
+
+                    DFS(next);
+                }
+            }
+
+            public void DFS2(int now)
+            {
+                Console.WriteLine(now);
+                visited[now] = true;
+
+                foreach (int next in adj2[now]) // 연결된 것만 확인
+                {
+                    if (visited[next]) // 이미 방문했다면 컨티뉴
+                        continue;
+
+                    DFS2(next);
+                }
+            }
+
+            public void SearchAll()
+            {
+                visited = new bool[6];
+                for (int now = 0; now < 6; now++)
+                {
+                    if (visited[now] == false)
+                        DFS(now);
+                }
+            }
         }
 
 
@@ -76,6 +120,9 @@ namespace Exercise
 
             // DFS (Depth First Search : 깊이 우선 탐색)
             // BFS (Breadth First Search : 너비 우선 탐색)
+
+            Graph graph = new Graph();
+            graph.SearchAll();
         }
     }
 }
