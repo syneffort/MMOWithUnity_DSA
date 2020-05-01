737bd56f7d79f8e6acd22751576c183a81d2a705
Pyunghwa Kim <syneffort@gmail.com>
2020-05-02 00:51:51 +0900

    [#18] 우선순위 큐

--- 0_Sources/Algorithm/Exercise_Tree/Program.cs
+++ 0_Sources/Algorithm/Exercise_Tree/Program.cs
@@ -1,24 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Exercise_Tree
 {
+    #region TreeNode
     class TreeNode<T>
     {
         public T Data { get; set; }
         public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
+    }
+    #endregion
+
+    class PriorityQueue<T> where T : IComparable<T>
+    {
+        List<T> _heap = new List<T>();
+
+        public void Push(T data)
+        {
+            // 힙 마지막에 데이터 삽입
+            _heap.Add(data);
+
+            int now = _heap.Count - 1;
+            // 도장깨기 시작
+            while (now > 0)
+            {
+                // 도장깨기 시도
+                int next = (now - 1) / 2;
+                if (_heap[now].CompareTo(_heap[next]) < 0)
+                    break;
+
+                // 두 값 교체
+                T temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+                now = next;
+            }
+        }
+
+        public T Pop()
+        {
+            // 반환할 데이터 저장
+            T ret = _heap[0];
+
+            // 마지막 데이터를 루트로 이동
+            int lastIndex = _heap.Count - 1;
+            _heap[0] = _heap[lastIndex];
+            _heap.RemoveAt(lastIndex);
+            lastIndex--;
+
+            // 역방향 도장깨기 시도
+            int now = 0;
+            while (true)
+            {
+                int left = 2 * now + 1;
+                int right = 2 * now + 2;
+
+                int next = now;
+                // 왼쪽 값이 현재값보다 크면 왼쪽으로 이동
+                if (left <= lastIndex && _heap[next].CompareTo(_heap[left]) < 0)
+                    next = left;
+
+                // 오른쪽 값이 현재값보다 크면 오른쪽으로 이동
+                if (right <= lastIndex && _heap[next].CompareTo(_heap[right]) < 0)
+                    next = right;
+
+                // 왼쪽 오른쪽 모두 현재 값보다 작으면 종료
+                if (next == now)
+                    break;
+
+                // 두 값을 교체
+                T temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+                now = next;
+            }
+
+            return ret;
+        }
+
+        public int Count
+        {
+            get { return _heap.Count; }
+        }
+    }
+
+    class Knight : IComparable<Knight>
+    {
+        public int Id { get; set; }
+
+        int IComparable<Knight>.CompareTo(Knight other)
+        {
+            if (Id == other.Id)
+                return 0;
+
+            return Id > other.Id ? 1 : -1;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            TreeNode<string> root = MakeTree();
+            #region TreeNode
+            //TreeNode<string> root = MakeTree();
             //PrintTree(root);
+            //Console.WriteLine(GetHeight(root));
+            #endregion
 
-            Console.WriteLine(GetHeight(root));
+            PriorityQueue<Knight> q = new PriorityQueue<Knight>();
+            q.Push(new Knight() { Id = 20 });
+            q.Push(new Knight() { Id = 30 });
+            q.Push(new Knight() { Id = 40 });
+            q.Push(new Knight() { Id = 10 });
+            q.Push(new Knight() { Id = 5 });
+
+            while (q.Count > 0)
+            {
+                Console.WriteLine(q.Pop().Id);
+            }
         }
 
+        #region TreeNode
         static TreeNode<string> MakeTree()
         {
             TreeNode<string> root = new TreeNode<string> { Data = "R1 개발실" };
@@ -75,5 +178,6 @@ namespace Exercise_Tree
 
             return height;
         }
+        #endregion
     }
 }
