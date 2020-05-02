2c10da7c84ed11f887df3b3ce266ef8dd1135680
Pyunghwa Kim <syneffort@gmail.com>
2020-05-02 16:15:28 +0900

    A Star 구현

--- 0_Sources/Algorithm/Algorithm/Player.cs
+++ 0_Sources/Algorithm/Algorithm/Player.cs
@@ -44,7 +44,115 @@ namespace Algorithm
             _board = board;
 
             //RightHand();
-            BFS();
+            //BFS();
+            AStar();
+        }
+
+        struct PQNode : IComparable<PQNode>
+        {
+            public int F;
+            public int G;
+            public int Y;
+            public int X;
+
+            int IComparable<PQNode>.CompareTo(PQNode other)
+            {
+                if (F == other.F)
+                    return 0;
+
+                return F < other.F ? 1 : -1;
+            }
+        }
+
+        void AStar()
+        {
+            // U L D R UL DL DR UR
+            //int[] deltaY = new int[] { -1, 0, 1, 0};
+            //int[] deltaX = new int[] { 0, -1, 0, 1};
+            //int[] cost = new int[] { 10, 10, 10, 10};
+            int[] deltaY = new int[] { -1, 0, 1, 0, -1, 1, 1, -1 };
+            int[] deltaX = new int[] { 0, -1, 0, 1, -1, -1, 1, 1 };
+            int[] cost = new int[] { 10, 10, 10, 10, 14, 14, 14, 14 };
+
+            // 점수 매기기
+            // F = G + H
+            // F = 최종점수
+            // G = 시작점에서 현재 좌표까지 이동하는데 필요한 비용 (작을수록 좋음, 경로에 따라 달라짐)
+            // H = 목적지에서 얼마나 가까운지 (작을수록 좋음, 고정)
+
+            // (y, x) 이미 방문했는지 여부
+            bool[,] closed = new bool[_board.Size, _board.Size];
+
+            // (y, x) 가는 길을 한번이라도 발견했는지 여부
+            int[,] open = new int[_board.Size, _board.Size];
+            for (int y = 0; y < _board.Size; y++)
+            {
+                for (int x = 0; x < _board.Size; x++)
+                {
+                    open[y, x] = int.MaxValue;
+                }
+            }
+
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+            // open 리스트 중 가장 좋은 후보를 뽑아내기 위한 우선순위큐
+            PriorityQueue<PQNode> pq = new PriorityQueue<PQNode>();
+
+            // 시작점 발견 (예약 진행)
+            open[PosY, PosX] = 10 * Math.Abs(_board.DestY - PosY) + Math.Abs(_board.DestX - PosX);
+            pq.Push(new PQNode() { F = 10 * Math.Abs(_board.DestY - PosY) + Math.Abs(_board.DestX - PosX), G = 0, Y = PosY, X = PosX });
+            parent[PosY, PosX] = new Pos(PosY, PosX);
+
+            while (pq.Count > 0)
+            {
+                // 제일 좋은 후보를 찾는다
+                PQNode node = pq.Pop();
+
+                // 동일한 좌표를 여러경로로 찾아서 이미 방문된 경우 스킵
+                if (closed[node.Y, node.X])
+                    continue;
+
+                // 방문함
+                closed[node.Y, node.X] = true;
+
+                // 도착치 도달 시 종료
+                if (node.Y == _board.DestY && node.X == _board.DestX)
+                    break;
+
+                // 상하좌우 등 이동할 수 있는 좌표인지 확인해서 예약
+                for (int i = 0; i < deltaY.Length; i++)
+                {
+                    int nextY = node.Y + deltaY[i];
+                    int nextX = node.X + deltaX[i];
+
+                    // 유효범위 확인
+                    if (nextY < 0 || nextY >= _board.Size || nextX < 0 || nextX >= _board.Size)
+                        continue;
+
+                    // 갈 수 있는 길인지 확인
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                        continue;
+
+                    // 이미 방문한 곳 확인
+                    if (closed[nextY, nextX])
+                        continue;
+
+                    // 비용 계산
+                    int g = node.G + cost[i];
+                    int h = 10 * Math.Abs(_board.DestY - nextY) + Math.Abs(_board.DestX - nextX);
+
+                    // 다른 경로에서 더 빠른 길이 있는지 확인
+                    if (open[nextY, nextX] < g + h)
+                        continue;
+
+                    // 예약 진행
+                    open[nextY, nextX] = g + h;
+                    pq.Push(new PQNode() { F = g + h, G = g, Y = nextY, X = nextX });
+                    parent[nextY, nextX] = new Pos(node.Y, node.X);
+                }
+            }
+
+            CalcPathFromParent(parent);
         }
 
         void BFS()
@@ -86,6 +194,11 @@ namespace Algorithm
                 }
             }
 
+            CalcPathFromParent(parent);
+        }
+
+        void CalcPathFromParent(Pos[,] parent)
+        {
             int y = _board.DestY;
             int x = _board.DestX;
             while (parent[y, x].Y != y || parent[y, x].X != x)
@@ -139,13 +252,18 @@ namespace Algorithm
             }
         }
 
-        const int MOVE_TICK = 10;
+        const int MOVE_TICK = 30;
         int _sumTick = 0;
         int _lastIndex = 0;
         public void Update(int deltaTick)
         {
             if (_lastIndex >= _points.Count)
-                return;
+            {
+                _lastIndex = 0;
+                _points.Clear();
+                _board.Initialize(_board.Size, this);
+                Initialize(1, 1, _board);
+            }
 
             _sumTick += deltaTick;
             if (_sumTick < MOVE_TICK)
